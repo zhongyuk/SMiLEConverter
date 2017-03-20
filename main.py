df3fb897c801b6dc5dec6016c5700ff2dc96b34e
Zhongyu Kuang <dream.kuang@gmail.com>
2017-03-20 15:12:22 -0700

    add .sh setting for acgan(non-debugged)

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ FLAGS = tf.flags.FLAGS
 tf.flags.DEFINE_integer("batch_size", "64", "batch size for training")
 tf.flags.DEFINE_string("logs_dir", "logs/CelebA_GAN_logs/", "path to logs directory")
 tf.flags.DEFINE_string("data_dir", "/Users/Zhongyu/Downloads/", "path to dataset")
+#tf.flags.DEFINE_string("data_dir", "/home/paperspace/Downloads/", "path to dataset")
 tf.flags.DEFINE_integer("z_dim", "100", "size of input vector to generator")
 tf.flags.DEFINE_float("learning_rate", "2e-4", "Learning rate for Adam Optimizer")
 tf.flags.DEFINE_float("optimizer_param", "0.5", "beta1 for Adam optimizer / decay for RMSProp")
---
+++ run_main_acgan.sh
@@ -0,0 +1,1 @@
+python main.py --logs_dir=logs/CelebA_ACGAN_logs/ --learning_rate=2e-4 --optimizer_param=0.5 --model=2 --iterations=2e3 --mode=train 
