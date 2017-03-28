5e04cd97d4400450317bb6f4f296ee967242f155
Zhongyu Kuang <dream.kuang@gmail.com>
2017-03-28 10:19:00 -0700

    update commands for new TF version

--- utils.py
+++ utils.py
@@ -160,18 +160,18 @@ def unprocess_image(image, mean_pixel, norm):
 
 def add_to_regularization_and_summary(var):
     if var is not None:
-        tf.histogram_summary(var.op.name, var)
+        tf.summary.histogram(var.op.name, var)
         tf.add_to_collection("reg_loss", tf.nn.l2_loss(var))
 
 
 def add_activation_summary(var):
-    tf.histogram_summary(var.op.name + "/activation", var)
+    tf.summary.histogram(var.op.name + "/activation", var)
     tf.scalar_summary(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
 
 
 def add_gradient_summary(grad, var):
     if grad is not None:
-        tf.histogram_summary(var.op.name + "/gradient", grad)
+        tf.summary.histogram(var.op.name + "/gradient", grad)
 
 def save_imshow_grid(images, logs_dir, filename, shape):
     """
